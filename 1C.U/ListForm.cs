c31f6fcc05fefcb5e1c68a96f7799f764376a92c
zhenya <zhenyaprokopev0@gmail.com>
2022-12-09 20:05:58 +0400

    основа

---
+++ 1C.U/ListForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1C.U
+{
+    public partial class ListForm : Form
+    {
+        public ListForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAdd(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonDelete(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonSort(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonFind(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
